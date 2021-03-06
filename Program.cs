68604af2cedc808db9bf124d5fbb6a3c293dd92d
Giorgio <giorgio160586@hotmail.com>
2021-03-06 17:19:15 +0100

    MongoDBConsoleApp

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MongoDBConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
